1b7b55dafe34bc2c35ee8e7cef46025c172717f6
Siarhei Drozd <drozd1586229@gmail.com>
2019-12-28 11:41:50 +0300

    fixed: ball size change

--- core/src/pl/ccoders/game/MyGdxGame.java
+++ core/src/pl/ccoders/game/MyGdxGame.java
@@ -6,16 +6,19 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
 	Texture ground;
 	Texture bubble;
-
+	int maxX = 20, maxY = 28;
+	float unitW, unitH;
 	float centerPosition;
-	float movingSpeed=10;
-
-
+	float movingSpeed=5;
+	float size;
+	float sizeIncSpeed;
+	boolean wasTouched, stop = false;
 
 	@Override
 	public void create () {
@@ -23,26 +26,33 @@ public class MyGdxGame extends ApplicationAdapter {
 		background = new Texture("background.jpg");
 		ground = new Texture("ground.png");
 		bubble = new Texture("bubble.png");
-		centerPosition=Gdx.graphics.getWidth()/2-150;
+		unitW = Gdx.graphics.getWidth()/maxX;
+		unitH = Gdx.graphics.getHeight()/maxY;
+		centerPosition=(maxX - 5)*unitW;
+		size = unitW;
+		sizeIncSpeed = unitW/25;
 	}
 
 	@Override
 	public void render () {
-		if(Gdx.input.justTouched()){
-			movingSpeed=0;
-		}else{
-			if (centerPosition <= Gdx.graphics.getWidth()-400 && centerPosition>150){
-				centerPosition-=movingSpeed;
-			}else if (centerPosition<150){
-				centerPosition=150;
-				centerPosition+=movingSpeed;
+		if(Gdx.input.isTouched() && !wasTouched && size < 8*unitW){
+			size+= sizeIncSpeed;
+			stop = true;
+		}
+		else if( !stop ){
+
+			if (centerPosition == unitW*(maxX-5)) {
+				movingSpeed = -5;
+			}
+			else if ( centerPosition == 5*unitW) {
+				movingSpeed = 5;
 			}
 
+			centerPosition += movingSpeed;
 		}
 		batch.begin();
-		batch.draw(background, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		batch.draw(ground, 0, 0,Gdx.graphics.getWidth(),150);
-		batch.draw(bubble, centerPosition, 50,300,200);
+		batch.draw(ground, 0, 0,unitW*maxX,150);
+		batch.draw(bubble, centerPosition-size/2, 50,size,size);
 		batch.end();
 	}
 	
