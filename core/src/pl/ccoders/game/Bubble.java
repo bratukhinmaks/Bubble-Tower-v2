24961cccf7276d7c3d5569235e30016000b13afc
Siarhei Drozd <drozd1586229@gmail.com>
2019-12-30 13:25:18 +0300

    bubble creating and size increasing fixed

--- core/src/pl/ccoders/game/Bubble.java
+++ core/src/pl/ccoders/game/Bubble.java
@@ -2,28 +2,33 @@ package pl.ccoders.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import java.lang.Math;
 
 public class Bubble {
     float size, sizeIncSpeed, posX, posY;
     boolean inMotion, inSizeInc;
     float vel, velSpeed;
+    float red,green,blue;
 
-    Texture bubble;
 
-    Bubble(float unitW, float posX, float posY) {
+    Bubble(float unit, float posX, float posY) {
         this.posX = posX;
         this.posY = posY;
         this.inMotion = true;
         this.inSizeInc = false;
-        this.bubble = new Texture("bubble.png");
-        this.sizeIncSpeed = unitW/25;
-        this.size = unitW;
+        this.sizeIncSpeed = unit/50;
+        this.size = unit/2;
         this.vel = 0f;
         this.velSpeed = 1/20f;
+        this.red = (float)Math.random();
+        this.blue = (float)Math.random();
+        this.green = (float)Math.random();
     }
 
-    void draw(SpriteBatch batch) {
-        batch.draw(bubble, posX, posY, size, size);
+    void draw(ShapeRenderer shape) {
+        shape.setColor(red, green, blue, 1);
+        shape.circle(posX, posY, size);
     }
 
     void move() {
--- core/src/pl/ccoders/game/MyGdxGame.java
+++ core/src/pl/ccoders/game/MyGdxGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 import java.util.ArrayList;
 
@@ -12,157 +13,54 @@ import pl.ccoders.game.Bubble;
 
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture ground;
-	Texture bubble;
-	int maxX = 20, maxY = 28;
-	float unitW, unitH;
-	float centerPosition;
-	float movingSpeed=5;
-	float size;
-	float sizeIncSpeed;
-	boolean wasTouched, stop = false;
-	Bubble currentBubble;
+	int maxUnits = 20;
+	float unit;
+	boolean wasTouched;
+	Bubble currentBubble, nextBubble;
 	ArrayList<Bubble> bubbles;
-	int index;
-	boolean wasTouch, createNext;
-	float currentPosX, currentPosY;
-	Bubble bubbleNext;
+	ShapeRenderer shape;
+
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		ground = new Texture("ground.png");
-		bubble = new Texture("bubble.png");
-		unitW = Gdx.graphics.getWidth()/maxX;
-		unitH = Gdx.graphics.getHeight()/maxY;
-		currentBubble = new Bubble(unitW, (maxX/2-3)*unitW,3*unitW);
-		currentBubble.size = 6*unitW;
+		unit = Gdx.graphics.getWidth()/maxUnits;
+		currentBubble = new Bubble(unit, maxUnits/2*unit,3/2*unit);
+		currentBubble.size = 3*unit;
 		currentBubble.inMotion = false;
-		bubbles = new ArrayList<Bubble>();
+		bubbles = new ArrayList<>();
 		bubbles.add(currentBubble);
-		currentPosX = currentBubble.posX - currentBubble.size;
-		currentPosY = currentBubble.posY - currentBubble.size/2 + unitW/2;
-		createNext = true;
-		wasTouch = false;
-		bubbleNext = new Bubble(unitW, currentPosX, currentPosY);
-		bubbles.add(bubbleNext);
-//		DONT DELETE THIS
-//		centerPosition=(maxX - 5)*unitW;
-//		size = unitW;
-//		sizeIncSpeed = unitW/25;
-//		tryBubble = new Bubble(unitW, unitW*10-3*unitW, unitW*3);
-//		tryBubble.size = 6*unitW;
-//		bubbles.add(tryBubble);
+		nextBubble = new Bubble(unit, currentBubble.posX-currentBubble.size, currentBubble.posY);
+		bubbles.add(nextBubble);
+		shape = new ShapeRenderer();
+		wasTouched = false;
 	}
 
 	@Override
 	public void render () {
-//		if(createNext && !wasTouch) {
-//			Bubble bubble = new Bubble(unitW, currentPosX, currentPosY);
-//			bubbles.add(bubble);
-//			if (Gdx.input.isTouched()) {
-//				wasTouch = true;
-//				bubble.speedY = 0;
-//				bubble.speedX = 0;
-//				bubble.size += bubble.sizeIncSpeed;
-//			}
-//			else if (bubble.posX == currentPosX && bubble.posY == currentPosY) {
-//				bubble.speedX = 5*unitW;
-//				bubble.speedY = 5*unitW;
-//			}
-//			else if(bubble.posX==currentPosX+currentBubble.size && bubble.posY==currentPosY) {
-//				bubble.speedX = -5*unitW;
-//				bubble.speedY = 5*unitW;
-//			}
-//			else if(bubble.posX==currentPosX+currentBubble.size/2 && bubble.posY == currentPosY+currentBubble.size/2) {
-//				bubble.speedY = -5*unitW;
-//			}
-//			else if( wasTouch ) {
-//				this.currentPosX = bubble.posX - bubble.size;
-//				this.currentPosY = bubble.posY - bubble.size/2+unitW/2;
-//				createNext = false;
-//			}
-//			bubble.posX += bubble.speedX;
-//			bubble.posY += bubble.speedY;
-//
-//		}
-//		else {
-//			createNext = true;
-//			wasTouch = false;
-//		}
-
-//		for ( int i = 1; i<=index; i++) {
-//			Bubble prev = bubbles.get(i-1);
-//			Bubble bubble = new Bubble(unitW, prev.posX-prev.size, prev.posY-prev.size/2+unitW/2);
-//			bubbles.add(bubble);
-//			if ( !Gdx.input.isTouched() && bubble.inMotion ) {
-//				if(bubble.posX == prev.posX-prev.size && bubble.posY == prev.posY-prev.size/2+unitW/2) {
-//					bubble.speedX = 5*unitW;
-//					bubble.speedY = 5*unitW;
-//				}
-//				else if(bubble.posX==prev.posX && bubble.posY==prev.posY-prev.size/2+unitW/2) {
-//					bubble.speedX = -5*unitW;
-//					bubble.speedY = 5*unitW;
-//				}
-//
-//				else if(bubble.posX==prev.posX+prev.size/2 && bubble.posY == prev.posY+prev.size) {
-//					bubble.speedY = -5*unitW;
-//				}
-//			}
-//			else if(bubble.inSizeInc && Gdx.input.isTouched()) {
-//				bubble.size += bubble.sizeIncSpeed;
-//			}
-//			else {
-//				bubble.inSizeInc = true;
-//				bubble.inMotion = false;
-//				++index;
-//			}
-//			bubble.posX += bubble.speedX;
-//			bubble.posY += bubble.speedY;
-//		}
-//      DONT DELETE THIS
-//
-		if(Gdx.input.isTouched() && !wasTouched && size < 8*unitW){
-			size+= sizeIncSpeed;
-			stop = true;
-		}
-		else if( !stop ){
-
-			if (centerPosition == unitW*(maxX-5)) {
-				movingSpeed = -5;
-			}
-			else if ( centerPosition == 5*unitW) {
-				movingSpeed = 5;
+		if(Gdx.input.isTouched()) {
+			wasTouched = true;
+			nextBubble.inMotion = false;
+			nextBubble.velSpeed = 0f;
+			if(nextBubble.size < 4*unit) {
+				nextBubble.size += nextBubble.sizeIncSpeed;
 			}
 
-			centerPosition += movingSpeed;
 		}
-		else if(stop) {
-			Bubble bubble1 = new Bubble(unitW, centerPosition, 6*unitW);
-			bubble1.size = 5*unitW;
-			bubbles.add(bubble1);
+		else if (!Gdx.input.isTouched() && wasTouched) {
+			currentBubble = nextBubble;
+			nextBubble = new Bubble(unit, currentBubble.posX-currentBubble.size, currentBubble.posY);
+			bubbles.add(nextBubble);
+			wasTouched = false;
 		}
-//		if( bubbles.get(1).vel < 0 ) {
-//			bubbles.get(1).velSpeed = 1/20f;
-//		}
-//		else if( bubbles.get(1).vel > 3.1415) {
-//			bubbles.get(1).velSpeed = -1/20f;
-//		}
-//		bubbles.get(1).vel += bubbles.get(1).velSpeed;
-		batch.begin();
-		batch.draw(ground, 0, 0,unitW*maxX,3*unitW);
-		currentBubble.draw(batch);
-		for(Bubble bubble: bubbles) {
-			if(!bubble.equals(currentBubble)) {
-				bubble.move();
-				if(bubble.posX < currentBubble.posX + currentBubble.size/2) bubble.posX = currentBubble.posX - bubble.size + currentBubble.size/2*(1-(float)Math.cos(bubble.vel));
-				else bubble.posX = currentBubble.posX + currentBubble.size/2*(1-(float)Math.cos(bubble.vel));
-				bubble.posY = currentBubble.posY + currentBubble.size/2*(1+(float)Math.sin(bubble.vel));
-			}
-			bubble.draw(batch);
+
+		shape.begin(ShapeRenderer.ShapeType.Filled);
+		nextBubble.move();
+		nextBubble.posX = currentBubble.posX - currentBubble.size*(float)Math.cos(nextBubble.vel)-nextBubble.size*(float)Math.cos(nextBubble.vel);
+		nextBubble.posY = currentBubble.posY + currentBubble.size*(float)Math.sin(nextBubble.vel)+nextBubble.size*(float)Math.sin(nextBubble.vel);
+		for( Bubble bubble: bubbles) {
+			bubble.draw(shape);
 		}
-		batch.end();
+		shape.end();
 
 	}
 	
